31bba4137ef5a45714491b96ea0c6223bdd2f8c5
Astrid Fayyaza Danishara <fayyazaastrid@gmail.com>
2026-09-24 14:31:40 +0700

    Integrasi koneksi database PostgreSQL dan fitur Google Sign-In

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-corountines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
